4919a1250c43f1a828537d1fbe5b0c278e0f58af
Christian Ashley <ashleycz1993@gmail.com>
2023-04-23 18:46:09 -0400

    init CTRI-15 demo
    function 'callResident' to randomly select from ctri15.residents array

--- JSONParser.js
+++ JSONParser.js
@@ -1,63 +1,138 @@
-function JSONParserWithSwitch(string) {
-  // Don't do this please 😭
-  switch (string) {
-    case 'true': 
-    case 'false':
-      return parseBool(string);
-    // this will never be true since the condition being checked
-    // is the whole string, you cannot isolate the first character
-    // in this method
-    case string[0] === '"':
-      return parseString(string);
-    default: 
-      return 'default';
-  };
-}
-
 function JSONParser(string) {
-  if (string === 'true' || string === 'false') return parseBool(string);
+  if (string === "null") return parseNull();
+  if (string === "true" || string === "false") return parseBool(string);
   if (string[0] === '"') return parseString(string);
-  if (string === 'null') return parseNull();
-  if (string[0] === '{' || string[0] === '[') return partitionData(string);
-}
-
-function parseBool(b) {
-  return b === 'true';
-}
-
-function parseString(s) {
-  // ???
-  return s.slice(1, -1).replace(/\\/g, "");
+  if (string[0] === "{" || string[0] === "[") return partitionData(string);
+  else return parseNum(string);
 }
 
 function parseNull() {
   return null;
 }
 
-function partitionData(s) {
-  const chunkArr = [];
-  let curChunk = '';
-  const brackStack = [];
-  const brackDict = { '{': '}', '[': ']'  };
-
-  // 0 and s.length-1 will be {}/[]
-  let count = 0;
-  for (let i = 1; i < s.length - 1; ++i) {
-    if (s[i] === '"') {
-      while(s[++i] !== '"') {
-        console.log(s[i])
-        curChunk += s[i];
-      }
-      console.log(++count, curChunk);
-      chunkArr.push()
-      curChunk = '';
-    }
-    console.log(s[i])
-  }
+function parseBool(string) {
+  return string === "true";
 }
 
-// console.log(JSONParser("null") === null)
-console.log(JSONParser(JSON.stringify({0: 'some string'})));
-console.log(JSONParser('"\ntest"'));
-// console.log('2');
-// console.log('{ "test": "value" }');
+function parseString(string) {
+  return string.slice(1, -1).replace(/\\(\\[bfnrtu])?/g, '$1');
+}
+
+function parseNum(string) {
+  return Number(string);
+}
+
+function partitionData(string) {
+  const bracketDict = { "[": "]", "{": "}" };
+  const bracketStack = [];
+  const chunks = [];
+  let currChunk = "";
+
+  for (let i = 1; i < string.length - 1; i++) {
+    if (string[i] === '"') {
+      currChunk += string[i++];
+      while (string[i] !== '"' && string[i - 1] !== "\\") {
+        currChunk += string[i++];
+      }
+    }
+
+    if (string[i] in bracketDict) {
+      bracketStack.push(string[i]);
+    }
+    else if (string[i] === bracketDict[bracketStack[bracketStack.length - 1]]) {
+      bracketStack.pop();
+    }
+    else if (string[i] === "," && bracketStack.length === 0) {
+      chunks.push(currChunk);
+      currChunk = "";
+      continue;
+    }
+
+    currChunk += string[i];
+
+    if (i === ( string.length - 1) - 1) {
+      chunks.push(currChunk);
+    }
+  }
+
+  return (string[0] === "[") ? parseArray(chunks) : parseObj(chunks);
+}
+
+function parseArray(chunks) {
+  return chunks.map((chunk) => JSONParser(chunk));
+}
+
+function parseObj(chunks) {
+  const outputObj = {};
+
+  for (let chunk of chunks) {
+    let key = '';
+    let val = '';
+
+    for (let i = 1; i < chunk.length; ++i) {
+      if (chunk[i] !== '"' && chunk[i - 1] !== "\\") {
+        key += chunk[i];
+      } else {
+        val = chunk.slice(i + 2);
+        break;
+      }
+    }
+    
+    outputObj[key] = JSONParser(val);
+  }
+  return outputObj;
+}
+
+const ctri15 = {
+  residents: [
+    'Adam Vanek',
+    'Akeem Smith',
+    'Alastair Scheuermann',
+    'Alejandro Florez',
+    'Anne-lise Emig',
+    'Carter Long',
+    'Christina Walton',
+    'Das Kang',
+    'Dylan Hawkins',
+    'Elena Attencio',
+    'Eric Dunn',
+    'Gary Balogh',
+    'Giovana De La Cruz',
+    'Iris Wong',
+    'Jackie Yuan',
+    'Jake Gillan',
+    'Jasmine Noor',
+    'John Donato',
+    'Jon Cruz',
+    // 'Jon Tan',
+    'Joseph Tejeda',
+    'Justin Dilts',
+    'Kasey Wolff',
+    'Luke Lauther',
+    'Mark Teets',
+    'Matteo Diterlizzi',
+    'Minzo Kim',
+    'Nicky Ly',
+    'Rachel Kucharski',
+    'Sam Lee',
+    'Sophia Bui',
+    'Stephen Havig',
+    'Timmy Zhu',
+    'Billy Murphy',
+    'YC Chiu',
+  ],
+  currentWeek: 2,
+  likes: ['"\nCostco\n"', 'Javascript', 'AJAX'],
+  alreadyPros: true,
+  weaknesses: null
+};
+
+// const ctri15String = JSON.stringify(ctri15).replace(/(\\[bfnrtu])/g, '\\' + '$1');
+// console.log(ctri15String)
+// console.log(JSONParser(ctri15String))
+
+function callResident() {
+  console.log('✨', ctri15.residents[Math.round(Math.random() * ctri15.residents.length)], '✨');
+};
+
+callResident();
